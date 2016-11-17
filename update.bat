b77a26ed6e558f10a483c8d5980f114abe66dcce
tasi788 <tasi788@gmail.com>
2016-11-18 00:14:09 +0800

    update.bat

--- update.bat
+++ update.bat
@@ -3,5 +3,5 @@ echo 本指令由低吸撰寫030
 echo 本指令只適用於預設模組位置
 @ping 127.0.0.1 -n 5 -w 3000 > nul
 cd tool
-wget --no-check-certificate https://raw.githubusercontent.com/tasi788/TDCsMineServer-1Click/master/tool/script.bat -O script.bat
+wget --no-check-certificate -q https://raw.githubusercontent.com/tasi788/TDCsMineServer-1Click/master/tool/script.bat -O script.bat
 script.bat
